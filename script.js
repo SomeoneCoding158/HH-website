d63b494c84e07756b9d7cd832cc6b8a408c1ac8f
SomeoneCoding158 <hergur@keystoneschool.org>
2026-09-02 15:14:43 -0500

    Added changes to script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-Object..defineProperty(window, 'unlock', {
-            get: function() {
-                window.location.href = "secret.html";
-                return "Redirecting...";
-            }
-});
+function secret() {
+    window.location.href = "secret.html";
+}
